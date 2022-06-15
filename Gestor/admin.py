0dfa68f25f7a17e9743f9011a4e22d8adbb9b82e
Erik <eri.torres@duocuc.cl>
2022-06-15 13:13:12 -0400

    as

--- Gestor/admin.py
+++ Gestor/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
-from .models import Tarea
 # Register your models here.
-
-admin.site.register (Tarea)
--- Gestor/models.py
+++ Gestor/models.py
@@ -1,11 +1,11 @@
-from django.db import models
+#from django.db import models
 
 # Create your models here.
-class usuario(models.Model):
-    Nombre_usuario = models.CharField(verbose_name='Nombre', max_length=100)
-    Rut = models.CharField(verbose_name="Rut", max_length=10)
-    correo_electronico = models.CharField (verbose_name='Nombre', max_length=100)
+#class usuario(models.Model):
+    #Nombre_usuario = models.CharField(verbose_name='Nombre', max_length=100)
+    #ut = models.CharField(verbose_name="Rut", max_length=10)
+    #correo_electronico = models.CharField (verbose_name='Nombre', max_length=100)
 
-class listadousuario(models.Model):
-    Rut = models.ForeignKey ()
+#class listadousuario(models.Model):
+   # Rut = models.ForeignKey ()
 
