e670132b9235f92f66cd2cc575854a152c54b7fa
davis <da.aranguiz@duocuc.cl>
2022-06-26 20:45:20 -0400

    cambios

--- gestor/views.py
+++ gestor/views.py
@@ -1,10 +1,14 @@
+from cgitb import html
 from django.shortcuts import render
 from .serializers import ProfSerializer
 from rest_framework import viewsets
-from .models import Profesion
+from .models import Profesion, Usuarios
 
 # Create your views here.
 class Profview(viewsets.ModelViewSet):
     serializer_class = ProfSerializer
     Queryset = Profesion.objects.all()
     
+def Inicio_sesion(request):
+    Usuarios = Inicio_sesion.objects.all();
+    return render(request,'gestor/Inicio_sesion.html',{'Usuarios': Usuarios})
