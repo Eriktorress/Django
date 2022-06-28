540646e2602fb59bd9d2c741b5629526d45552a8
Nacho <ig.dinamarca@duocuc.cl>
2022-06-28 12:26:44 -0400

    Añadiendo views y comentandolas

--- gestor/views.py
+++ gestor/views.py
@@ -10,10 +10,30 @@ class Profview(viewsets.ModelViewSet):
     serializer_class = ProfSerializer
     Queryset = Profesion.objects.all()
 
-def home(request):
+# PAGINAS CON LO INICIAL 
+
+def home(request):  #PAGINA 1
     return render(request, 'gestor/home.html')
 
+def Inicio_sesion(request):  #PAGINA 2
+    return render(request,'Inicio_sesion.html')
 
+def Dashboard(request):  #PAGINA 3
+    return render(request,'Dashboard.html') 
+
+
+# PAGINAS CON METODOS DINAMICOS
+
+#USUARIOS
+def Listado_usuarios (request):
+    return render (request,'Listado_usuarios.html')
+
+def Formulario_Usuario (request):
+    return render(request,'Formulario_Usuario.html')
+
+
+
+#TRABAJADORES
 def list_trab(request):
     listado = Trabajadores.objects.all();
     return render(request, 'gestor/list_trab.html', {'listado':listado})
